22f46b626ac7e5bd1bdc990503ba9a51c016056b
Ulzahk <ulzahk@gmail.com>
2021-09-07 05:51:15 -0500

    [UPDATE] Added test for arrays, numbers, boolean and callbacks

--- jsbase/src/__tests__/global.test.js
+++ jsbase/src/__tests__/global.test.js
@@ -1,5 +1,27 @@
 const text = "Hello World";
+const fruits = ["Manzana", "Banana", "Melon"];
+const reverseString = (string, callback) => {
+  callback(string.split("").reverse().join(""));
+};
 
-test("Should contain a text", () => {
+test("It should contain a text", () => {
   expect(text).toMatch(/World/);
+});
+
+test("It should have a banana", () => {
+  expect(fruits).toContain("Banana");
+});
+
+test("It should be greater value", () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test("It should return a true value", () => {
+  expect(true).toBeTruthy();
+});
+
+test("It should be a callback ", () => {
+  reverseString("Hello", (str) => {
+    expect(str).toBe("olleH");
+  });
 });
