2cb24104ef0069348c530a6034b7dd27f4353708
Ulzahk <ulzahk@gmail.com>
2021-09-08 06:40:15 -0500

    [UPDATE] Added test for reducers

---
+++ platzi-store/src/__tests__/reducers/reducers.test.js
@@ -0,0 +1,36 @@
+import reducer from '../../reducers';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Reducers', () => {
+  test('Return initial State ', () => {
+    expect(reducer({}, '')).toEqual({});
+  });
+  test('Check ADD_TO_CART', () => {
+    const initialState = {
+      cart: [],
+    };
+    const payload = ProductMock;
+    const action = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+    const expected = {
+      cart: [ProductMock],
+    };
+    expect(reducer(initialState, action)).toEqual(expected);
+  });
+  test('Check REMOVE_FROM_CART', () => {
+    const initialState = {
+      cart: [ProductMock],
+    };
+    const payload = ProductMock;
+    const action = {
+      type: 'REMOVE_FROM_CART',
+      payload,
+    };
+    const expected = {
+      cart: [],
+    };
+    expect(reducer(initialState, action)).toEqual(expected);
+  });
+});
