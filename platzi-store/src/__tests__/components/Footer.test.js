cc9896ea894b079e3b086aa7a5eda418833513f5
Ulzahk <ulzahk@gmail.com>
2021-09-08 05:27:47 -0500

    [UPDATE] Initial jest configuration for a project and created first test

---
+++ platzi-store/src/__tests__/components/Footer.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Footer from '../../components/Footer';
+
+describe('<Footer />', () => {
+  const footer = mount(<Footer />);
+  test('Footer component render ', () => {
+    expect(footer.length).toEqual(1);
+  });
+  test('Title render', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
